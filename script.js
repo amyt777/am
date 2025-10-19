0a32de6510dec70f47d72f891714874a3fb0af79
amyt777 <arman.x.pk@gmail.com>
2025-10-19 15:38:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Show bottom message after 5 seconds
 window.addEventListener('load', () => {
+    // Show bottom message after 5 seconds
     setTimeout(() => {
         const msg = document.getElementById('bottomMessage');
         msg.style.bottom = '30px';
         msg.style.opacity = '1';
-        // Hide after 5 seconds
+        // Hide it again smoothly after 5 seconds
         setTimeout(() => {
             msg.style.bottom = '-50px';
             msg.style.opacity = '0';
